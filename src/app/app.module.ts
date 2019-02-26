4f29f57bf7527c3bb7918992d85e945c547fa375
AnjaliTB <anjalitbaiju@gmail.com>
2019-02-26 10:49:41 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomePage } from './home/home.page';
+import { RegisterPage } from './register/register.page';
+
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { RegisterPage } from '../register/register.page';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  username:string;
+  password:string;
+  reppass:string;
+constructor(public navCtrl:NavController){
 
+}
+showAlert() {
+  mobiscroll.alert({
+      title: 'Cellular Data is Turned Off for "Safari"',
+      message: 'You can turn on cellular data for this app in Settings.',
+      callback: function () {
+          mobiscroll.toast({
+              message: 'Alert closed'
+          });
+      }
+  });
+}
+login(){
+  console.log("Username"+this.username);
+  console.log("Password"+this.password);
+  console.log("Repeat Password"+this.reppass);
+  if(this.username=='anju' && this.password=='anju')
+    console.log("Login successful");
+  else
+    console.log("Login unsuccessful");
+
+}
+goRegister(){
+  this.navCtrl.navigateForward('./register/register.page.html');
+}
 }
